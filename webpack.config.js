92ba1f0d35a6f36bb73e63af8e2169d297dffd3a
violafranco <franco5viola@gmail.com>
2026-06-09 15:29:40 -0300

    fix: remove CleanWebpackPlugin, add transpileOnly to ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,15 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import TerserPlugin from "terser-webpack-plugin";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// transpileOnly: true evita que ts-loader intente re-emitir declaraciones
+// (las declaraciones las genera tsc --emitDeclarationOnly por separado)
 const tsRule = {
   test: /\.tsx?$/,
-  use: "ts-loader",
+  use: { loader: "ts-loader", options: { transpileOnly: true } },
   exclude: /node_modules/,
 };
 
@@ -26,9 +27,8 @@ const umdMin = {
   output: {
     filename: "quantidia-sdk.umd.min.js",
     path: path.resolve(__dirname, "dist"),
-    library: { name: "Quantidia", type: "window" }, // window.Quantidia
+    library: { name: "Quantidia", type: "window" },
   },
-  plugins: [new CleanWebpackPlugin()],
   optimization: { minimize: true, minimizer: [new TerserPlugin()] },
 };
 
